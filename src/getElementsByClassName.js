ae56e4c9832aec7a4c92a3dd36de3bffce111072
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-05 12:58:37 -0700

    Working on test page "fill_me_in"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,32 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var input = document.body.children;//return an array of nodes
+  var result = [];
+
+  //arguments,
+  if (arguments.length === 2) {
+    input = arguments[1];
+  }
+  console.log('className: ', className);
+
+  for (var i = 0; i < input.length; i++) {
+    // need conditional to isolate className from other info
+    var currentNode = input[i];
+    if (currentNode.className === className) {
+      result.push(currentNode);
+    }
+
+    // if hasChildNodes, iterate, find more className
+    if (currentNode.hasChildNodes()) {
+      var theKids = currentNode.children;
+      if (Array.isArray(theKids)) {
+        for (var j = 0; j < theKids.length; j++) {
+          getElementsByClassName(className, theKids[j]);
+        }
+      }
+    }
+  }
+  console.log('result: ', result);
+  return result;
 };
